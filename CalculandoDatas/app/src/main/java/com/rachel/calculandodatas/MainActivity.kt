8ddf265bb8cb93e3e454c57ad7f35bafb762d70c
Rachel Barros <rachelnmlb@hotmail.com>
2021-05-18 10:07:34 -0300

    Adicionando Lista de presentes

--- CalculandoDatas/app/src/main/java/com/rachel/calculandodatas/MainActivity.kt
+++ CalculandoDatas/app/src/main/java/com/rachel/calculandodatas/MainActivity.kt
@@ -1,17 +1,14 @@
 package com.rachel.calculandodatas
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.DatePicker
 import android.widget.EditText
 import android.widget.TextView
-import java.time.Instant
+import androidx.appcompat.app.AppCompatActivity
 import java.time.LocalDate
-import java.time.Period
-import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
-import java.time.temporal.TemporalUnit
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var txtMesagem : TextView
     lateinit var inputDataNascimento : DatePicker
     lateinit var inputPresente : EditText
+    val listaPresentes = mutableSetOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,13 +37,13 @@ class MainActivity : AppCompatActivity() {
             var dataNascimento = LocalDate.of(inputDataNascimento.year, inputDataNascimento.month + 1, inputDataNascimento.dayOfMonth)
             var presente = inputPresente?.text.toString()
             var intervaloDias = calcularDias(dataNascimento)
-
+            var mensagem : String
             if (nome.isEmpty()) nome = "Desconhecido"
             if (presente.isEmpty()) presente = "Nada"
 
-            txtMesagem.text =
-                    "Olá $nome, faltam $intervaloDias dias para o seu aniversário! Espero que você ganhe um $presente!"
+            mensagem = "Olá $nome, faltam $intervaloDias dias para o seu aniversário! Espero que você ganhe um $presente!"
 
+            cadastrarPresente(mensagem)
         }
     }
 
@@ -57,5 +55,21 @@ class MainActivity : AppCompatActivity() {
             proxAniversario = proxAniversario.plusYears(1)
         }
         return ChronoUnit.DAYS.between(dataHoje, proxAniversario)
+    }
+
+    fun cadastrarPresente( mensagem : String) {
+
+        txtMesagem.visibility = View.VISIBLE
+        listaPresentes.add(mensagem)
+
+        var elemento = ""
+        var i = 1
+
+        for(item in listaPresentes) {
+            elemento += "$i- $item \n"
+            i++
+        }
+
+        txtMesagem.text = elemento
     }
 }
